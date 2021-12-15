f1ba481c1b167cee407688f62821c8a122c2a760
Mayank Kataria <mynkkataria@gmail.com>
2021-12-15 19:01:11 +0530

    isAdmin MultiDictKeyError resolved

--- erp_attendance/views.py
+++ erp_attendance/views.py
@@ -13,14 +13,15 @@ def login(request):
     return render(request, 'login.html')
 
 def addEmployee(request):
+    print('----------------------------------add employee-----------------------------------------')
     firstname = request.POST['firstname']
     lastname = request.POST['lastname']
     username = request.POST['username']
     empid = request.POST['empid']
     email = request.POST['email']
     department = request.POST['department']
-    isAdmin = request.POST['isAdmin'] == 'isAdmin'
-    print('--------------isAdmin', isAdmin)
+    isAdmin = request.POST.get('isAdmin') == 'on'
+    print('-------------------------------------isAdmin-----------------------------------------', isAdmin)
     user = User.objects.create_user(username, email, 'Arcgate1!')
     user.is_superuser = isAdmin
     user.first_name = firstname
