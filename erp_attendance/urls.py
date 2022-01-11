99c42b0c5903e497d3f8003e3d57bdd454d7e0aa
Priyanshu <priyanshulasod773@gmail.com>
2022-01-11 12:11:57 +0530

    stage junk

--- erp_attendance/urls.py
+++ erp_attendance/urls.py
@@ -5,9 +5,15 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.showLogin, name='showLogin'),
+    path('login/', views.loginUser, name='loginUser'),
+    path('logout/', views.logoutUser, name='logoutUser'),
+    # path('admin-panel/<int:id>', views.adminPanel, name='adminPanel'),
+    path('admin-panel/', views.adminPanel, name='adminPanel'),
     path('add-employee/', views.addEmployee, name='addEmployee'),
     path('edit-employee/<int:id>', views.editEmployee, name='editEmployee'),
     path('delete-employee/<int:id>', views.deleteEmployee, name='deleteEmployee'),
     path('show-employee/<int:id>', views.showEmployee, name='showEmployee'),
+    path('attendance-sheet/<int:id>', views.addAttendance, name='addAttendance'),
+    path('add-attendance/<int:id>',views.addAttendance, name = 'addAttendance'),
 ]
